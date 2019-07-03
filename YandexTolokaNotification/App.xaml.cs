c8b4dcabb90821a5d2bb1bac04ae3aba5f72a5cf
UltWolf <ultwolf@gmail.com>
2019-07-03 13:12:25 +0300

    Add minimize to tray

--- YandexTolokaNotification/App.xaml.cs
+++ YandexTolokaNotification/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,19 @@ namespace TolokaNotification
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+             
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
     }
 }
---
+++ YandexTolokaNotification/ModelView/NotifyIconViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using YandexTolokaNotification.Services.Abstracts;
+using YandexTolokaNotification.Services.Commands;
+
+namespace YandexTolokaNotification.ModelView
+{ 
+        public class NotifyIconViewModel:BaseViewModel
+        { 
+            /// <summary>
+            /// Shows a window, if none is already open.
+            /// </summary>
+            public ICommand ShowWindowCommand
+            {
+                get
+                {
+                    return new RelayCommand((object obj) =>
+                    {
+                        Application.Current.MainWindow = new Login();
+                        Application.Current.MainWindow.Visibility = Visibility.Visible;
+                    }, (object obj) => Application.Current.MainWindow.IsVisible==false);
+                }
+                
+            }
+
+            /// <summary>
+            /// Hides the main window. This command is only enabled if a window is open.
+            /// </summary>
+            public ICommand HideWindowCommand
+            {
+            get
+            {
+                return new RelayCommand((object obj) => { Application.Current.MainWindow.Visibility = Visibility.Hidden; }, (object obj) =>  Application.Current.MainWindow != null);
+  
+                }
+            }
+
+
+            /// <summary>
+            /// Shuts down the application.
+            /// </summary>
+            public ICommand ExitApplicationCommand
+            {
+                get
+                {
+                    return new RelayCommand( (object obj) => Application.Current.Shutdown() );
+                }
+            }
+        }
+    }
+
